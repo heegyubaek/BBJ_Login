394e13773ec4c38c61dfcfbca1aadd17e81324db
benjamin <bhg.king47@gmail.com>
2022-01-30 17:18:24 +0900

    First commit for bbj_login module

---
+++ src/main/java/com/bbj/bbj_login/BbjLoginApplication.java
@@ -0,0 +1,13 @@
+package com.bbj.bbj_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbjLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbjLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbj/bbj_login/BbjLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbj.bbj_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbjLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
